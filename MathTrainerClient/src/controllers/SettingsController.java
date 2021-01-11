414d6808b85ee3b1fb88ddd7e5ccc99da71ad1c7
Gavin Nejsum <g.nejsum@gmail.com>
2021-01-11 14:13:30 +0100

    quick change

--- MathTrainerClient/src/controllers/SettingsController.java
+++ MathTrainerClient/src/controllers/SettingsController.java
@@ -49,42 +49,39 @@ public class SettingsController extends MainMenuControllerParent implements Init
 				year.getSelectionModel().select(0);
 			} else if(user.getYear() == 7) {
 				year.getSelectionModel().select(1);
-			}else if(user.getYear() == 8) {
-				year.getSelectionModel().select(2);
-			}else if(user.getYear() == 9) {
-				year.getSelectionModel().select(3);
+
+			} else {
+				userInfoLabel.setText("");
+				// Ifall användaren är inte inloggad gör komponentera osynliga.
+				setComponentsVisible(false);
 			}
-		} else {
-			userInfoLabel.setText("");
-			// Ifall användaren är inte inloggad gör komponentera osynliga.
-			setComponentsVisible(false);
 		}
 	}
 
-	public void setComponentsVisible(boolean choice) {
-		settingsLabel.setVisible(choice);
-		passwordLabel.setVisible(choice);
-		password.setVisible(choice);
-		schoolLabel.setVisible(choice);
-		school.setVisible(choice);
-		cityLabel.setVisible(choice);
-		city.setVisible(choice);
-		yearLabel.setVisible(choice);
-		year.setVisible(choice);
-		changeSettings.setVisible(choice);
-	}
+		public void setComponentsVisible(boolean choice) {
+			settingsLabel.setVisible(choice);
+			passwordLabel.setVisible(choice);
+			password.setVisible(choice);
+			schoolLabel.setVisible(choice);
+			school.setVisible(choice);
+			cityLabel.setVisible(choice);
+			city.setVisible(choice);
+			yearLabel.setVisible(choice);
+			year.setVisible(choice);
+			changeSettings.setVisible(choice);
+		}
 
-	public void changeSettings(ActionEvent actionEvent) {
-		if(password.getText().toString().length() >= 6 ) {
-			boolean confirm = mainController.popUpWindow(Alert.AlertType.CONFIRMATION, "Inställningar",
-					"Är du säker på att du vill ändra dina uppgifter?");
-			if (confirm) {
-				mainController.changeUser(user.getUserName(), password.getText(), city.getText(), school.getText(), year.getValue());
-				mainController.setScene(ScenesEnum.Home);
+		public void changeSettings(ActionEvent actionEvent) {
+			if(password.getText().toString().length() >= 6 ) {
+				boolean confirm = mainController.popUpWindow(Alert.AlertType.CONFIRMATION, "Inställningar",
+						"Är du säker på att du vill ändra dina uppgifter?");
+				if (confirm) {
+					mainController.changeUser(user.getUserName(), password.getText(), city.getText(), school.getText(), year.getValue());
+					mainController.setScene(ScenesEnum.Home);
+				}
+			} else {
+				mainController.popUpWindow(Alert.AlertType.ERROR, "Lösenordet är för kort",
+						"Lösenordet måste vara minst 6 tecken långt.");
 			}
-		} else {
-			mainController.popUpWindow(Alert.AlertType.ERROR, "Lösenordet är för kort",
-					"Lösenordet måste vara minst 6 tecken långt.");
 		}
 	}
-}
